ad2bb821174c1a06905b28edaff740bd8ca67347
FaithChepkemoi <faithhope017@gmail.com>
2025-01-28 13:41:34 +0300

    function dargdropcontainer

--- src/Components/DragDropContainer.js
+++ src/Components/DragDropContainer.js
@@ -1,3 +1,11 @@
 // importing the necessary content from the downloaded react-beautify-dnd
  import { DragDropContext,Droppable,Draggable } from "react-beautiful-dnd";
- 
+ //importing libraries
+  import React,{useState,useEffect} from "react";
+  import axios from 'axios'; // used for making http request to fetch data from the server 
+
+ function DragDropContainer (){
+     const [tasks,setTasks] = useState([]);
+     
+
+ }
