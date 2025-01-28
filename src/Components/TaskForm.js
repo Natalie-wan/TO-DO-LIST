d5b3a888a00458fdac6783e09e6da1deb5b851e5
Natalie <wanjikunatalie631@gmail.com>
2025-01-28 15:12:24 +0300

    states to manage form inputs

---
+++ src/Components/TaskForm.js
@@ -0,0 +1,9 @@
+import React, { use, useState } from "react";
+
+function TaskForm({ onAddTask }) {
+    //States to manage form inputs
+    const [title, setTitle] = useState("");
+    const [category, setCategory] = useState("");
+    const [priority, setPriority] = useState("");
+    const [dueDate, setDueDate] = useState("");
+}
