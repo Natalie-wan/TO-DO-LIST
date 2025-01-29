bce1747ca9edd2f6d84fdc6a12fa14e51480a1c8
FaithChepkemoi <faithhope017@gmail.com>
2025-01-30 00:47:21 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-import React from "react";
-import "./App.css";
-import DragDropContainer from "./Components/DragDropContainer";
-=======
 import React, { useState } from 'react';
 import TaskForm from './Components/TaskForm';
 import TaskList from './Components/TaskList';
 //import './App.css';
->>>>>>> 2eb41d9189f88dbd8da62bc70781a6a90529a71a
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -26,16 +20,10 @@ function App() {
     .catch((error) => console.error("Error:", error));
   };
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <h1>Todo List App</h1>
-      <DragDropContainer />
-=======
     <div>
       <h1>To-Do List</h1>
       <TaskForm onAddTask={handleAddTask} />
       <TaskList tasks={tasks} />
->>>>>>> 2eb41d9189f88dbd8da62bc70781a6a90529a71a
     </div>
   );
 }
