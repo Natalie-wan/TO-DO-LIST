4cf286c40ae74a0819d7af283916af76647742de
Issa Mohamed <issa.mohamed@student.moringaschool.com>
2025-01-29 22:07:05 +0300

    modifying CategoryFilter component for filtering tasks by category

---
+++ mockTasks.js
@@ -0,0 +1,40 @@
+// mockTasks.js
+const mockTasks = [
+    {
+      id: 1,
+      title: "Read a book",
+      category: "Leisure",
+      priority: "Low",
+      dueDate: "2024-02-10",
+    },
+    {
+      id: 2,
+      title: "Prepare for meeting",
+      category: "Work",
+      priority: "High",
+      dueDate: "2024-02-05",
+    },
+    {
+      id: 3,
+      title: "Plan a weekend trip",
+      category: "Personal",
+      priority: "Medium",
+      dueDate: "2024-02-15",
+    },
+    {
+      id: 4,
+      title: "Morning yoga",
+      category: "Health",
+      priority: "Medium",
+      dueDate: "2024-02-07",
+    },
+    {
+      id: 5,
+      title: "Pay utility bills",
+      category: "Finance",
+      priority: "High",
+      dueDate: "2024-02-03",
+    },
+  ];
+  
+  export default mockTasks;
