df237ac2271d28bc72931c03cd64ac001916d845
FaithChepkemoi <faithhope017@gmail.com>
2025-01-29 11:34:03 +0300

    droppable

--- src/Components/DragDropContainer.js
+++ src/Components/DragDropContainer.js
@@ -7,16 +7,32 @@
  function DragDropContainer (){
      const [tasks,setTasks] = useState([]);
 
+     //useEffects that fetch tasks when component mounts
       useEffect(() =>{
-        axios.get('http://localhost:3001/tasks')
-         .then (response =>{
+        axios.get('http://localhost:3001/tasks') // Get request fetching tasks from the server
+         .then (response =>{  
+            //update the tasks state with the fetched data
              setTasks(response.data);
          })
 
          .catch(error=>{
+            //log any error that occurs during the request
             console.error(error);
          });
 
-      }, []);
+      }, []); //empty dependancies arrays ensure to run once on mount 
+
+
+       const handleDragEnd = (result) => {
+        if (!result.destination) return;
+
+        const { source, destination } =result;
+
+        if (source.droppableId !== destination.droppableId) {
+            return;
+        }
+        
+
+       }
 
  }
